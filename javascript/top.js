b1aa0ac679588f28486afccb33bdc40e19ca853a
Cadmus79 <42519971+Cadmus79@users.noreply.github.com>
2021-01-31 01:35:31 +0000

    Segundo Commit
    New Commit

--- javascript/top.js
+++ javascript/top.js
@@ -1,34 +1,30 @@
 var cloneMedia = $('.media').clone();
 
-$('#btSearch').on('click', function(){
+/*$('#btSearch').on('click', function(){
 
 	var valorPesquisa = $('#pesquisa').val();
 	$('.panel-title').text('Resultados da pesquisa de "' + valorPesquisa + '"');
-
+*/
 	$('.media-list').html('');
 
 	$.ajax({
 		method: "GET",
-		url: "http://ws.audioscrobbler.com/2.0/?method=track.search&limit=10&track=" + valorPesquisa + "&api_key=9ce20f16bde36a8f5734ac6f62463ebe&format=json"
+        url: "http://ws.audioscrobbler.com/2.0/?method=geo.gettoptracks&country=portugal&limit=10&api_key=9ce20f16bde36a8f5734ac6f62463ebe&format=json"
 	})
 	.done(function(msg){
 		//console.log('msg');
 		console.log(msg);
 
-		msg.results.trackmatches.track.forEach(function(result){
+		msg.tracks.track.forEach(function(result){
 			var liMedia = cloneMedia.clone();
 
 			$('a', liMedia).attr('href', result.url);
 			$('#image', liMedia).attr('src', result.image[2]["#text"])
 			$('.title', liMedia).text(result.name);
-			$('.Artista', liMedia).text(result.artist);
+			$('.Artista', liMedia).text(result.artist.name);
 			$('.Ouvintes', liMedia).text(result.listeners);
 			
 			$('.media-list').append(liMedia);
 		})
-	})
 });
 
-function togglePopup(){
-	document.getElementById("first-popup").classList.toggle("active");
-}
